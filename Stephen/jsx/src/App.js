c6d4b9c012f16abf58510e11238dc6be09453ecb
nhatkytran <nhatky.tran.2002@gmail.com>
2023-03-03 09:12:55 +0700

    React D6 - Props

--- Stephen/jsx/src/App.js
+++ Stephen/jsx/src/App.js
@@ -2,14 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const name = "Ky";
-  const age = 21;
-
-  return (
-    <h1>
-      My name is {name} and my age is {age}
-    </h1>
-  );
+  return <h1>Hello {">"}</h1>;
 }
 
 export default App;
